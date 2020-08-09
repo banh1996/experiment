3da6748754935a333b05065ac7224a5438a46795
banh1996 <banh1809@gmail.com>
2020-08-09 22:08:43 +0700

    start ss5

---
+++ ss5/main.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// Size of the buffer
+#define SIZE 5
+
+// Special marker used to indicate end of the producer data
+#define END_MARKER -1
+
+// Buffer, shared resource
+int buffer[SIZE];
+// Number of items in the buffer, shared resource
+int count = 0;
+// Index where the producer will put the next item
+int prod_idx = 0;
+// Index where the consumer will pick up the next item
+int con_idx = 0;
+// How many items will be produced
+int num_iterations = 0;
+
+// Initialize the mutex
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Initialize the condition variables
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+
+/*
+ Produce an item. Produces a random integer between [0, 1000] unless it is the last item to be produced in which case the value -1 is returned.
+*/
+int produce(int i)
+{
+    int value;
+    if (i == num_iterations - 1)
+        value = END_MARKER;
+    else
+        value = rand() % 1000;
+    buffer[prod_idx] = value;
+    // Increment the index where the next item will be put. Roll over to the start of the buffer if the item was placed in the last slot in the buffer
+    prod_idx = (prod_idx + 1) % SIZE;
+    count++;
+    return value;
+}
+
+/*
+ Function that the producer thread will run. Produce an item and put in the buffer only if there is space in the buffer. If the buffer is full, then wait until there is space in the buffer.
+*/
+void *producer(void *args)
+{
+    for (int i = 0; i < num_iterations; i++)
+    {
+      // Lock the mutex before checking where there is space in the buffer
+      pthread_mutex_lock(&mutex);
+      while (count == SIZE)
+        // Buffer is full. Wait for the consumer to signal that the buffer has space
+        pthread_cond_wait(&empty, &mutex);
+      printf("PROD %d\n", produce(i));
+      // Signal to the consumer that the buffer is no longer empty
+      pthread_cond_signal(&full);
+      // Unlock the mutex
+      pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+/*
+ Get the next item from the buffer
+*/
+int consume()
+{
+    int value = buffer[con_idx];
+    // Increment the index from which the item will be picked up, rolling over to the start of the buffer if currently at the end of the buffer
+    con_idx = (con_idx + 1) % SIZE;
+    count--;
+    return value;
+}
+
+/*
+ Function that the consumer thread will run. Get  an item from the buffer if the buffer is not empty. If the buffer is empty then wait until there is data in the buffer.
+*/
+void *consumer(void *args)
+{
+    int value = 0;
+    // Continue consuming until the END_MARKER is seen    
+    while (value != END_MARKER)
+    {
+      // Lock the mutex before checking if the buffer has data      
+      pthread_mutex_lock(&mutex);
+      while (count == 0)
+        // Buffer is empty. Wait for the producer to signal that the buffer has data
+        pthread_cond_wait(&full, &mutex);
+      value = consume();
+      printf("CONS %d\n", value);
+      // Signal to the producer that the buffer has space
+      pthread_cond_signal(&empty);
+      // Unlock the mutex
+      pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+      printf("Usage: ./main num_iterations\n");
+      printf("\tProvide number of iterations for the program as an integer greater than 0\n");
+      exit(1);
+    }
+    srand(time(0));
+    num_iterations = atoi(argv[1]);
+    // Create the producer and consumer threads
+    pthread_t p, c;
+    pthread_create(&p, NULL, producer, NULL);
+    // Sleep for a few seconds to allow the producer to fill up the buffer. This has been put in to demonstrate the the producer blocks when the buffer is full. Real-world systems won't have this sleep    
+    sleep(5);
+    pthread_create(&c, NULL, consumer, NULL);
+    pthread_join(p, NULL);
+    pthread_join(c, NULL);
+    return 0;
+}
---
+++ ss5/main.rs
@@ -0,0 +1,158 @@
+use std::env; // to get arugments passed to the program
+
+/*
+* Print the number of partitions and the size of each partition
+* @param vs A vector of vectors
+*/
+fn print_partition_info(vs: &Vec<Vec<usize>>){
+    println!("Number of partitions = {}", vs.len());
+    for i in 0..vs.len(){
+        println!("\tsize of partition {} = {}", i, vs[i].len());
+    }
+}
+
+/*
+* Create a vector with integers from 0 to num_elements -1
+* @param num_elements How many integers to generate
+* @return A vector with integers from 0 to (num_elements - 1)
+*/
+fn generate_data(num_elements: usize) -> Vec<usize>{
+    let mut v : Vec<usize> = Vec::new();
+    for i in 0..num_elements {
+        v.push(i);
+    }
+    return v;
+}
+
+/*
+* Partition the data in v into a 2 vectors
+* @param num_partitions
+* @param v 
+* @return A vector that contains vectors of integers
+
+*/
+fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>{
+    let partition_size = v.len() / 2;
+    let mut xs: Vec<Vec<usize>> = Vec::new();
+
+    let mut x1 : Vec<usize> = Vec::new();
+    for i in 0..partition_size{
+        x1.push(v[i]);
+    }
+    xs.push(x1);
+
+    let mut x2 : Vec<usize> = Vec::new();
+    for i in partition_size..v.len(){
+        x2.push(v[i]);
+    }
+    xs.push(x2);
+
+    xs
+}
+
+/*
+* Sum up the all the integers in the given vector
+* @param v Vector of integers
+* @return Sum of integers in v
+* Note: this function has the same code as the reduce_data function.
+*       But don't change the code of map_data or reduce_data.
+*/
+fn map_data(v: &Vec<usize>) -> usize{
+    let mut sum = 0;
+    for i in v{
+        sum += i;
+    }
+    sum
+}
+
+/*
+* Sum up the all the integers in the given vector
+* @param v Vector of integers
+* @return Sum of integers in v
+*/
+fn reduce_data(v: &Vec<usize>) -> usize{
+    let mut sum = 0;
+    for i in v{
+        sum += i;
+    }
+    sum
+}
+
+/*
+* A single threaded map-reduce program
+*/
+fn main() {
+
+    // Use std::env to get arguments passed to the program
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("ERROR: Usage {} num_partitions num_elements", args[0]);
+        return;
+    }
+    let num_partitions : usize = args[1].parse().unwrap();
+    let num_elements : usize = args[2].parse().unwrap();
+    if num_elements < num_partitions{
+        println!("ERROR: num_elements cannot be smaller than num_partitions");
+        return;
+    }
+
+    // Generate data.
+    let v = generate_data(num_elements);
+
+    // PARTITION STEP: partition the data into 2 partitions
+    let xs = partition_data_in_two(&v);
+
+    // Print info about the partitions
+    print_partition_info(&xs);
+
+    let mut intermediate_sums : Vec<usize> = Vec::new();
+
+    // MAP STEP: Process each partition
+
+    // CHANGE CODE START: Don't change any code above this line
+
+    // Change the following code to create 2 threads each of which must use map_data()
+    // function to process one of the two partition
+
+    intermediate_sums.push(map_data(&xs[0]));
+    intermediate_sums.push(map_data(&xs[1]));
+
+    // CHANGE CODE END: Don't change any code below this line until the next CHANGE CODE comment
+
+    // Print the vector with the intermediate sums
+    println!("Intermediate sums = {:?}", intermediate_sums);
+
+    // REDUCE STEP: Process the intermediate result to produce the final result
+    let sum = reduce_data(&intermediate_sums);
+    println!("Sum = {}", sum);
+
+    // CHANGE CODE: Add code that does the following:
+    // 1. Calls partition_data to partition the data into equal partitions
+    // 2. Calls print_partiion_info to print info on the partitions that have been created
+    // 3. Creates one thread per partition and uses each thread to process one partition
+    // 4. Collects the intermediate sums from all the threads
+    // 5. Prints information about the intermediate sums
+    // 5. Calls reduce_data to process the intermediate sums
+    // 6. Prints the final sum computed by reduce_data
+
+}
+
+/*
+* CHANGE CODE: code this function
+* Note: Don't change the signature of this function
+*
+* Partitions the data into a number of partitions such that
+*   - the returned partitions contain all elements that are in the input vector
+*   - all partitions (except possibly the the last one) have equal number 
+*      of elements. The last partition may have either the same number of
+*      elements or fewer elements than the other partitions.
+*
+* @param num_partitions The number of partitions to create
+* @param v The data to be partitioned
+* @return A vector that contains vectors of integers
+* 
+*/
+fn partition_data(num_partitions: usize, v: &Vec<usize>) -> Vec<Vec<usize>>{
+    // Remove the following line which has been added to remove a compiler error
+    partition_data_in_two(v)
+}
