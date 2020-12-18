590853c32d9dc75bc11b1beb438147a0016e9fd2
Tran Ngoc Hung <hungtn16@viettel.com.vn>
2020-12-18 04:16:26 +0000

    ok

--- C_interview/q1.c
+++ C_interview/q1.c
@@ -5,121 +5,102 @@
 
 #define BUF_LEN  50
 
-static char input[BUF_LEN] = {1, 1, 0, 1, 0, 0, 1, 1, 0, 0};
-
-static int compare_substrings(char *a1, char *a2, int len)
+typedef struct node_t
 {
+    char *p_data;
+    struct node_t *next;
+} node_t;
+
+// static char input[BUF_LEN] = {1, 0, 1, 0, 1, 1, 1, 0, 0, 0};
+// static char input[BUF_LEN] = {1, 1, 0, 1, 1, 0, 0, 0};
+// static char input[BUF_LEN] = {1, 1, 0, 0};
+static char input[BUF_LEN] = {'1', '1', '0', '0', '1', '1', '1', '0', '0', '0', '1', '1', '1', '1', '0', '0', '1', '0', '0', '0'};
+
+static int compare_substrings(char *a1, char *a2)
+{
+    int len;
+    if (strlen(a1) > strlen(a2))
+        return 0;
+    else if (strlen(a1) < strlen(a2))
+        return 1;
+    len = strlen(a1);
     for (int i = 0; i < len; i++)
-    {
+    {   
         if (a1[i] > a2[i])
             return 0;
         if (a1[i] < a2[i])
             return 1;
-    }
+    }   
     return 0;
 }
 
-static char *swap_substrings(char *a, int first, int index, int last, int len)
+static node_t *find_substring(char *a)
 {
-    char *buf;
-    buf = (char*)malloc(len*sizeof(char*));
-    memcpy(buf, a, len);
-    for (int i = index; i >= first; i--)
-    {
-        buf[last-index+i] = a[i];
-    }
-
-    for (int i = index+1; i <= last; i++)
-    {
-        buf[first+i-index-1] = a[i];
-    }
-
-    return buf;
-}
-
-static char *get_max_number_substring(char *a, int index, int len)
-{
-    int i, j, count1 = 0, count0 = 0, first = index, last = index+1;
-    char *p_result, *ptr;
-    p_result = (char*)malloc(len*sizeof(char*));
-    memset(p_result, 0, len);
-    memcpy(p_result, a, len);
-    while (first != 0 || last != len-1)
-    {
-        count1 = 0;
-        count0 = 0;
-        for (i = first; i >= 0; i--)
-        {
-            if (a[i])
-                count1++;
-            else
-                count0++;
-            if (count1 > count0)
-                break;
-            if (count1 == count0)
-            {
-                first = i;
-                break;
-            }
-        }
-        if (count1 != count0)
-            break;
-
-        count1 = 0;
-        count0 = 0;
-        for (i = last; i < len; i++)
-        {
-            if (a[i])
-                count1++;
-            else
-                count0++;
-            if (count1 == count0)
-            {
-                last = i;
-                break;
-            }
-        }
-        if (count1 != count0)
-            break;
-
-        ptr = swap_substrings(a, first, index, last, len);
-
-        if (compare_substrings(a, ptr, len))
-        {
-            memcpy(p_result, ptr, len);
-            free(ptr);
-        }
-    }
-    return p_result;
-}
-
-static void print_result(char *a, int len)
-{
+    int count0 = 0, count1 = 0, index = 0, len = strlen(a);
+    node_t *p_node = NULL, *temp_p_node = NULL;
+    p_node = malloc(sizeof(node_t*));
+    temp_p_node = p_node;
     for (int i = 0; i < len; i++)
-        printf("%d ", a[i]);
+    {
+        if (a[i] == '1')
+        {
+            count1++;
+        }
+        else
+        {
+            count0++;
+        }
+
+        if (count1 == count0)
+        {
+            count1 = 0;
+            count0 = 0;
+            p_node->p_data = malloc((i+1)*sizeof(char*));
+            memcpy(p_node->p_data, &a[index], i - index + 1);
+            printf("test %s\n", p_node->p_data);
+            p_node->next = malloc(sizeof(node_t*));
+            p_node = p_node->next;
+            index = i + 1;
+        }
+    }
+    return temp_p_node;
+}
+
+static node_t *sort_substring(node_t *head_node)
+{
+    node_t *p_i, *p_j;
+    char *p_temp_data;
+    for (p_i = head_node; p_i->next->next != NULL; p_i = p_i->next)
+    {
+        for (p_j = p_i->next; p_j->next != NULL; p_j = p_j->next)
+        {
+            if (compare_substrings(p_i->p_data, p_j->p_data))
+            {
+                p_temp_data = p_i->p_data;
+                p_i->p_data = p_j->p_data;
+                p_j->p_data = p_temp_data;
+            }
+        }
+    }
+    return head_node;
+}
+
+static void print_result(node_t *head_node)
+{
+    node_t *p_i;
+    for (p_i = head_node; p_i->next != NULL; p_i = p_i->next)
+    {
+        for (int i = 0; i < strlen(p_i->p_data); i++)
+        {
+            printf("%c ", p_i->p_data[i]);
+        }
+    }
     printf("\n");
 }
 
 void main(void)
 {
-    int len = 10;
-    char *p_result, *ptr;
+    char *p_result;
     p_result = input;
-    for (int i = 1; i < len; i++)
-    {
-        ptr = get_max_number_substring(input, i, len);
-        if (compare_substrings(p_result, ptr, len))
-        {
-            if (p_result != input)
-                free(p_result);
-            p_result = ptr;
-        }
-        else
-        {
-            free(ptr);
-        }
-    }
-    print_result(p_result, len);
-    if (p_result != input)
-        free(p_result);
+    print_result(sort_substring(find_substring(p_result)));
 }
