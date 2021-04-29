633f18d42ca46b643dd4cee63b034df3c2be6e9e
Tran Ngoc Hung <v.hungtn9@vantixinc.com>
2021-04-29 21:30:13 +0700

    wt

--- documents/CS342/HW9/answer_hw9.pl
+++ documents/CS342/HW9/answer_hw9.pl
@@ -19,8 +19,8 @@ accCp([],[]).
 accCp([H|T1],[H|T2]) :- accCp(T1,T2).
 
 bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
-	b_sort([],Acc,Acc).
-	b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
+b_sort([],Acc,Acc).
+b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
 bubble(X,[],[],X).
 bubble(X,[Y|T],[Y|NT],Max):-X>Y,bubble(X,T,NT,Max).
 bubble(X,[Y|T],[X|NT],Max):-X=<Y,bubble(Y,T,NT,Max).
@@ -33,8 +33,8 @@ duplicate(X, A, Z) :-
     duplicate_help(X, A, X1), bubble_sort(X1, Z).
 
 %q3
-%check_condition([x,y|z], r) :- x == 10, y == 20, z == x+y, true.
-%check_condition([x,y|z], r) :- x == 10, y > 20, z == x-y, true.
+%check_condition(x, y, z, r) :- x == 10, y == 20, z = x+y.
+%check_condition([x,y|z], r) :- x == 10, y > 20, z = x-y.
 insertAt(Element,0,L,[Element|L]).
 insertAt(Element,Pos,[E|L],[E|ZL]):-
     Pos1 is Pos-1,
@@ -44,23 +44,51 @@ insertAt(Element,Pos,[E|L],[E|ZL]):-
 
 
 %q4, A,B,C,D,E is the finished job time.
-:- use_module(library(clpfd)).
+del([X|XS], CNT, [X|RES]) :- NEXT is CNT - 1,
+                             del(XS, NEXT, RES).
+del([_|XS], 1, XS).
 
-zmin_deleted(Zs1,Zs0) :-
-   same_length(Zs1,[_|Zs0]),
-   maplist(#=<(Min),Zs1),
-   select(Min,Zs1,Zs0).
+indexOf([Element|_], Element, 0).
+indexOf([_|Tail], Element, Index):-
+  indexOf(Tail, Element, Index1),
+  Index is Index1+1.
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
+replace(L, _, _, L).
+q4_solve(X) :- A = 0, B is A+1, C is A-1, D is B-1, E is D+1,
+     IN=[A,B,C,D,E],
+	 bubble_sort(IN, Z),
+    nth0(0, Z, Q0), indexOf(IN, Q0, Index0),
+    		(Index0==0 -> (write('A-'), replace(IN, 0, 11, IN0));
+    		(Index0==1 -> (write('B-'), replace(IN, 1, 11, IN0));
+			(Index0==2 -> (write('C-'), replace(IN, 2, 11, IN0));
+    		(Index0==3 -> (write('D-'), replace(IN, 3, 11, IN0));
+    		(Index0==4 -> (write('E-'), replace(IN, 4, 11, IN0))))))),
+    nth0(1, Z, Q1), indexOf(IN0, Q1, Index1),
+    		(Index1==0 -> (write('A-'), replace(IN0, 0, 11, IN1));
+    		(Index1==1 -> (write('B-'), replace(IN0, 1, 11, IN1));
+			(Index1==2 -> (write('C-'), replace(IN0, 2, 11, IN1));
+    		(Index1==3 -> (write('D-'), replace(IN0, 3, 11, IN1));
+    		(Index1==4 -> (write('E-'), replace(IN0, 4, 11, IN1))))))),
+    nth0(2, Z, Q2), indexOf(IN1, Q2, Index2),
+    		(Index2==0 -> (write('A-'), replace(IN1, 0, 11, IN2));
+    		(Index2==1 -> (write('B-'), replace(IN1, 1, 11, IN2));
+			(Index2==2 -> (write('C-'), replace(IN1, 2, 11, IN2));
+    		(Index2==3 -> (write('D-'), replace(IN1, 3, 11, IN2));
+    		(Index2==4 -> (write('E-'), replace(IN1, 4, 11, IN2))))))),
+    nth0(3, Z, Q3), indexOf(IN2, Q3, Index3),
+    		(Index3==0 -> (write('A-'), replace(IN2, 0, 11, IN3));
+    		(Index3==1 -> (write('B-'), replace(IN2, 1, 11, IN3));
+			(Index3==2 -> (write('C-'), replace(IN2, 2, 11, IN3));
+    		(Index3==3 -> (write('D-'), replace(IN2, 3, 11, IN3));
+    		(Index3==4 -> (write('E-'), replace(IN2, 4, 11, IN3))))))),
+    nth0(4, Z, Q4), indexOf(IN3, Q4, Index4),
+    		(Index4==0 -> (write('A'));
+    		(Index4==1 -> (write('B'));
+			(Index4==2 -> (write('C'));
+    		(Index4==3 -> (write('D'));
+    		(Index4==4 -> (write('E'))))))).
 
-minl([Only], Only).
-minl([Head|Tail], Minimum) :-
-    minl(Tail, TailMin),
-    Minimum is min(Head, TailMin).
-
-%q4_solve(X) :- A=0, B is A+1, C is A-1, D is B-1, E is D+1, 
-%    min is minl([A,B,C,D,E]), bubble_sort([A,B,C,D,E],X).
-
-
-
-
+%q5
 
 
